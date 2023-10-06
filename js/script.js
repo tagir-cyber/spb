4d019b9511f4a34ae5b115f1126dc87c9a8b686d
Тагир <tagir@MacBook-Pro-Tagir.local>
2023-10-06 13:12:08 +0300

    Исправлены кнопки в слайдере

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    nextButton: '.city__left-btn',
-    nextButton: '.swiper-button-prev',
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    // navigation: {
-        // enable:true,
-        // nextButton: '.city__left-btn',
-        // nextButton: '.swiper-button-prev',
-    // },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const leftBtn = document.getElementById("left-btn");
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //     nextButton: '.city__left-btn',
+  //     nextButton: '.swiper-button-prev',
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".city__right-btn",
+    prevEl: ".city__left-btn",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
